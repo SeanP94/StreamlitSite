20508b52720260ceb0ecee6ec0ea9c2bda9075c1
Sean <seanep94@gmail.com>
2023-06-04 00:49:32 -0700

    More intro stuff, final push for the night

--- README.md
+++ README.md
@@ -8,6 +8,9 @@
 https://github.com/blackary/st_pages
 for the sidebar.
 
+Look into setting the projects parameters to set sizing of the sidebar and possible the website itself.
+
+
 Section of front page.
 1|2
  __
--- main.py
+++ main.py
@@ -3,19 +3,23 @@ import streamlit as st
 imgCol, introCol = st.columns([.3,.7])
 # Display PFP
 
-
+# Header code
 imgCol.image("pfp.jpg")
 introCol.write("*-Intro-*")
 for _ in range(9): introCol.write("")
 introCol.write(":::Links to socials:::")
 st.divider()
 
+# Summary code
 summaryContainer = st.container()
+summaryContainer.header("Introduction")
 summaryContainer.write("Summarize my interests in the field of data")
 summaryContainer.code("print('hello world')", language="python")
 
+# Website code
 st.divider()
 siteExplanation  = st.container()
+siteExplanation.header("Why I Made This Website")
 siteExplanation.write("Explain usage of website and such.")
 
 
