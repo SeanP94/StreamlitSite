e1fba5c7628a3652a3013eebe6e733c9f76fe1ec
Sean <seanep94@gmail.com>
2023-06-04 23:26:39 -0700

    Added social media button links

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
 # This file is temporary, Will be transfered to a JSON file after I've completed the formatting.
 
 homePage = {
-
+    "myIntroduction" : """
+    
+    """,
 }
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+# Main page configuration.
 st.set_page_config(
     page_icon=":octopus:",
     page_title="SP Portfolio Site",
@@ -14,21 +15,32 @@ imgCol, introCol = st.columns([.3,.7])
 imgCol.image("pfp.jpg")
 introCol.write("*-Intro-*")
 for _ in range(9): introCol.write("")
-introCol.write(":::Links to socials:::")
+
+# Using shields.io for the buttons.
+introCol.write('''
+<p align="center">
+    <a href="https://www.linkedin.com/in/seanp1994/">
+        <img src="https://img.shields.io/badge/LinkedIn-blue?style=flat&logo=linkedin" alt="Linkedin">
+    </a>
+    <a href="https://leetcode.com/sperry1994/">
+        <img src="https://img.shields.io/badge/LeetCode-blue?style=flat&logo=leetcode" alt="Linkedin">
+    </a>S
+</p>
+''', unsafe_allow_html=True)
+
 st.divider()
 
 # Summary code
 summaryContainer = st.container()
 summaryContainer.header("Introduction")
 summaryContainer.write("Summarize my interests in the field of data")
-summaryContainer.code("print('hello world')", language="python")
+
 
 # Website code
 st.divider()
 siteExplanation  = st.container()
 siteExplanation.header("Why I Made This Website")
 siteExplanation.write("Explain usage of website and such.")
-
 
 
 ######## SideBar ########
